7f04a39bb9b9baf8aaddee48faf06103f6adb6a0
Adam Mihelcsik <adam.mihelcsik@ibm.com>
2022-02-23 18:23:25 +0000

    don't set closeReqSent flag if the backend.send fails

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -470,11 +470,9 @@ func (s *ProxyServer) serveRecvFrontend(stream client.ProxyService_ProxyServer, 
 				// TODO: retry with other backends connecting to this agent.
 				klog.ErrorS(err, "CLOSE_REQ to Backend failed", "serverID", s.serverID, "connectionID", connID)
 			} else {
-				klog.V(5).InfoS("CLOSE_REQ sent to backend", "serverID", s.serverID, "connectionID", connID)
+				klog.V(5).Infoln("CLOSE_REQ sent to backend", "serverID", s.serverID, "connectionID", connID)
+				closeReqSent = true
 			}
-			closeReqSent = true
-			klog.V(5).Infoln("CLOSE_REQ sent to backend", "serverID", s.serverID, "connectionID", connID)
-
 		case client.PacketType_DIAL_CLS:
 			random := pkt.GetCloseDial().Random
 			klog.V(5).InfoS("Received DIAL_CLOSE", "serverID", s.serverID, "dialID", random)
