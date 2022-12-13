5c75f97b62ee13017bba2554ad9841ee34d0ab21
Joseph Anttila Hall <jkh@google.com>
2022-12-13 10:32:18 -0800

    konnectivity-client: add dial failures metric by reason.

--- konnectivity-client/pkg/client/metrics/metrics.go
+++ konnectivity-client/pkg/client/metrics/metrics.go
@@ -41,29 +41,67 @@ type ClientMetrics struct {
 	registerOnce  sync.Once
 	streamPackets *prometheus.CounterVec
 	streamErrors  *prometheus.CounterVec
+	dialFailures  *prometheus.CounterVec
 }
 
+type DialFailureReason string
+
+const (
+	DialFailureUnknown DialFailureReason = "unknown"
+	// DialFailureTimeout indicates the hard 30 second timeout was hit.
+	DialFailureTimeout DialFailureReason = "timeout"
+	// DialFailureContext indicates that the context was cancelled or reached it's deadline before
+	// the dial response was returned.
+	DialFailureContext DialFailureReason = "context"
+	// DialFailureEndpoint indicates that the konnectivity-agent was unable to reach the backend endpoint.
+	DialFailureEndpoint DialFailureReason = "endpoint"
+	// DialFailureDialClosed indicates that the client received a CloseDial response, indicating the
+	// connection was closed before the dial could complete.
+	DialFailureDialClosed DialFailureReason = "dialclosed"
+	// DialFailureTunnelClosed indicates that the client connection was closed before the dial could
+	// complete.
+	DialFailureTunnelClosed DialFailureReason = "tunnelclosed"
+)
+
 func newMetrics() *ClientMetrics {
+	// The denominator (total dials started) for both
+	// dial_failure_total and dial_duration_seconds is the
+	// stream_packets_total (common metric), where segment is
+	// "from_client" and packet_type is "DIAL_REQ".
+	dialFailures := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: Namespace,
+			Subsystem: Subsystem,
+			Name:      "dial_failure_total",
+			Help:      "Number of dial failures observed, by reason (example: remote endpoint error)",
+		},
+		[]string{
+			"reason",
+		},
+	)
 	return &ClientMetrics{
 		streamPackets: commonmetrics.MakeStreamPacketsTotalMetric(Namespace, Subsystem),
 		streamErrors:  commonmetrics.MakeStreamErrorsTotalMetric(Namespace, Subsystem),
+		dialFailures:  dialFailures,
 	}
 }
 
 // RegisterMetrics registers all metrics with the client application.
-func (c *ClientMetrics) RegisterMetrics(r prometheus.Registerer, namespace, subsystem string) {
+func (c *ClientMetrics) RegisterMetrics(r prometheus.Registerer) {
 	c.registerOnce.Do(func() {
 		r.MustRegister(c.streamPackets)
 		r.MustRegister(c.streamErrors)
+		r.MustRegister(c.dialFailures)
 	})
 }
 
 // LegacyRegisterMetrics registers all metrics via MustRegister func.
 // TODO: remove this once https://github.com/kubernetes/kubernetes/pull/114293 is available.
-func (c *ClientMetrics) LegacyRegisterMetrics(mustRegisterFn func(...prometheus.Collector), namespace, subsystem string) {
+func (c *ClientMetrics) LegacyRegisterMetrics(mustRegisterFn func(...prometheus.Collector)) {
 	c.registerOnce.Do(func() {
 		mustRegisterFn(c.streamPackets)
 		mustRegisterFn(c.streamErrors)
+		mustRegisterFn(c.dialFailures)
 	})
 }
 
@@ -71,6 +109,11 @@ func (c *ClientMetrics) LegacyRegisterMetrics(mustRegisterFn func(...prometheus.
 func (c *ClientMetrics) Reset() {
 	c.streamPackets.Reset()
 	c.streamErrors.Reset()
+	c.dialFailures.Reset()
+}
+
+func (c *ClientMetrics) ObserveDialFailure(reason DialFailureReason) {
+	c.dialFailures.WithLabelValues(string(reason)).Inc()
 }
 
 func (c *ClientMetrics) ObservePacket(segment commonmetrics.Segment, packetType client.PacketType) {
