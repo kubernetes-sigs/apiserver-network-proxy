879e6640c269526edc5f41f5921b9375b9ec749d
Willow Carretero Chavez <carreter@google.com>
2024-06-18 16:24:32 +0000

    Add static and cached server counters

---
+++ pkg/servercounter/counter.go
@@ -0,0 +1,14 @@
+package servercounter
+
+// A ServerCounter counts the number of available proxy servers.
+type ServerCounter interface {
+	CountServers() int
+}
+
+// A StaticServerCounter stores a static server count.
+type StaticServerCounter int
+
+// CountServers returns the current (static) proxy server count.
+func (sc StaticServerCounter) CountServers() int {
+	return int(sc)
+}
